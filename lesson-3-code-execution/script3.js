c4858979ed46a089bc4866cc228ebcbc62768ad1
Pavel Kladkevich <kladpav@gmail.com>
2022-11-02 14:09:40 +0300

    lesson-3 task3 complete

---
+++ lesson-3-code-execution/script3.js
@@ -0,0 +1,23 @@
+var digits = [];
+var digit;
+var i = 0;
+
+do {
+  digit = prompt('Введите числа. После каждого числа нажмите OK. Завершить ввод - пустая строка или Cancel');
+  if ((digit === '') || (digit === null)) {
+    digit = NaN;
+  } else {
+    digit = Number(digit);
+  }
+  if (!(isNaN(digit))) {
+    digits[i] = digit;
+    i++;
+  }
+} while (!(isNaN(digit)));
+
+/* Далее находим сумму всех элементов массива. По умолчанию здесь, скорее всего, я бы использовал цикл. Но, так как во
+ время занятия было предложено попробовать обойтись без цикла, решение будет таким: */
+var result = digits.reduce(function (sum, current) {
+  return sum + current;
+}, 0);
+alert(`Сумма всех введенных чисел равна ${result}`);
