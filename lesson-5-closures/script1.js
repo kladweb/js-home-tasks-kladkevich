4e34020101eb8af366e3d04f470b07f5b117543f
Pavel Kladkevich <kladpav@gmail.com>
2022-11-18 00:22:31 +0300

    lesson-5 task challenge 5 complete

--- lesson-5-closures/script1.js
+++ lesson-5-closures/script1.js
@@ -1,5 +1,6 @@
 'use strict';
 var john = {
+  name: 'John',
   bills: [124, 48, 268, 180, 42],
   tips: [],
   finalValues: [],
@@ -7,42 +8,65 @@ var john = {
     var percentage;
     for (var i = 0; i < this.bills.length; i++) {
       if (this.bills[i] < 50) {
-        percentage = 0.2;
-      } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
-        percentage = 0.15;
+        percentage = 20;
+      } else if (this.bills[i] >= 50 && this.bills[i] <= 200) {
+        percentage = 15;
       } else {
-        percentage = 0.1;
+        percentage = 10;
       }
-      this.tips[i] = this.bills[i] * percentage;
+      this.tips[i] = this.bills[i] * percentage / 100;
       this.finalValues[i] = this.bills[i] + this.tips[i];
     }
     return this.tips;
   }
 };
 john.tipCalc();
-console.log('Аll tips: ' + john.tips);
-console.log('Final paid amounts (bill + tip): ' + john.tips);
+console.log('---------------------------------------');
+console.log('Аll John\'s tips: ' + john.tips);
+console.log('Final paid John\'s amounts (bill + tip): ' + john.finalValues);
 
-/*
-function tipCalculator(bill) {
-  var percentage;
-  if (bill < 50) {
-    percentage = 0.2;
-  } else if (bill >= 50 && bill < 200) {
-    percentage = 0.15;
-  } else {
-    percentage = 0.1;
+var mark = {
+  name: 'Mark',
+  bills: [77, 375, 110, 45],
+  tips: [],
+  finalValues: [],
+  tipCalc: function () {
+    var percentage;
+    for (var i = 0; i < this.bills.length; i++) {
+      if (this.bills[i] < 100) {
+        percentage = 20;
+      } else if (this.bills[i] >= 100 && this.bills[i] <= 300) {
+        percentage = 15;
+      } else {
+        percentage = 10;
+      }
+      this.tips[i] = this.bills[i] * percentage / 100;
+      this.finalValues[i] = this.bills[i] + this.tips[i];
+    }
+    return this.tips;
   }
-  return percentage * bill;
+};
+mark.tipCalc();
+console.log('---------------------------------------');
+console.log('Аll Mark\'s tips: ' + mark.tips);
+console.log('Final paid Mark\'s amounts (bill + tip): ' + mark.finalValues);
+
+function averageTip(client) {
+  var tipSum = 0;
+  for (var i = 0; i < client.tips.length; i++) {
+    tipSum += client.tips[i];
+  }
+  return tipSum / client.tips.length;
 }
 
-var bills = [124, 48, 268];
-var tips = [tipCalculator(bills[0]),
-  tipCalculator(bills[1]),
-  tipCalculator(bills[2])];
-
-var finalValues = [bills[0] + tips[0],
-  bills[1] + tips[1],
-  bills[2] + tips[2]];
-
-console.log(tips, finalValues);*/
+var avarageJohn = averageTip(john);
+var avarageMark = averageTip(mark);
+console.log('---------------------------------------');
+if (avarageJohn > avarageMark) {
+  console.log(john.name + '\'s family paid the highest tips on average of $' + avarageJohn);
+} else if (avarageJohn < avarageMark) {
+  console.log(mark.name + '\'s family paid the highest tips on average of $' + avarageMark);
+} else {
+  console.log(`Families ${mark.name}\s and ${mark.name}\s paid the same tips on average of $${avarageJohn}`);
+}
+console.log('---------------------------------------');
