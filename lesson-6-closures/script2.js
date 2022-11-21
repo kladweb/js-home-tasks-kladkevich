13a6b5acda066dd74b26c39a1f57da463518afaf
Pavel Kladkevich <kladpav@gmail.com>
2022-11-21 15:33:36 +0300

    lesson-6 task2 new version2

--- lesson-6-closures/script2.js
+++ lesson-6-closures/script2.js
@@ -1,6 +1,27 @@
 'use strict';
 
 function interviewQuestion(profession) {
+  if (profession === 'designer') {
+    return function (name) {
+      return `${name} can you please explain what UX design is?`;
+    }
+  }
+  if (profession === 'teacher') {
+    return function (name) {
+      return `What subject do you teach ${name}?`;
+    }
+  }
+  return function (name) {
+    return `Hello ${name}, what do you do?`;
+  }
+}
+
+console.log(interviewQuestion('teacher')('John'));
+
+/*
+//Первоначальный вариант
+
+function interviewQuestion(profession) {
   return function (name) {
     if (profession === 'designer') {
       return `${name} can you please explain what UX design is?`;
@@ -11,5 +32,5 @@ function interviewQuestion(profession) {
     return `Hello ${name}, what do you do?`;
   }
 }
-
 console.log(interviewQuestion('teacher')('John'));
+*/
